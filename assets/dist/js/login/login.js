f93eed85856056b7db62eb101ca6ef11b4261a1a
miamor <miamorwest@gmail.com>
2017-12-04 22:52:36 +0700

    04.12.17

--- assets/dist/js/login/login.js
+++ assets/dist/js/login/login.js
@@ -90,7 +90,7 @@ window.fbAsyncInit = function() {
 // successful.  See statusChangeCallback() for when this call is made.
 function testAPI() {
   console.log('Welcome!  Fetching your information.... ');
-  FB.api('/me', function(response) {
+  FB.api('/me', {fields: 'name, email'}, function(response) {
     console.log('Successful login for: ' + response.name);
     console.log(response);
     //document.getElementById('status').innerHTML = 'Thanks for logging in, ' + response.name + '!';
