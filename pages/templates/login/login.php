a9246f821313dc20aeffeb5f03b92e07f977b6af
miamor <miamorwest@gmail.com>
2017-12-04 22:40:54 +0700

    04.12.17

--- pages/templates/login/login.php
+++ pages/templates/login/login.php
@@ -21,7 +21,9 @@
     </div>
 </form>
 <div class="col-lg-3">
-    <div class="fb-login-button" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false"></div>
+    <!--<div class="fb-login-button" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false"></div>-->
+    <fb:login-button scope="public_profile,email" onlogin="checkLoginState();"></fb:login-button>
+    <div id="status"></div>
 </div>
 <div class="col-lg-1"></div>
 <div class="clearfix"></div>
