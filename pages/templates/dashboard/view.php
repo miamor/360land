4471c15b2b94e7e3d6d0a1ee176357ecfb5e4db7
miamor <miamorwest@gmail.com>
2017-12-12 15:57:29 +0700

    11.12.17

--- pages/templates/dashboard/view.php
+++ pages/templates/dashboard/view.php
@@ -1,10 +1,23 @@
 <h2 class="page-title show">Dashboard</h2>
 
 
-<div class="col-lg-7">
+<div class="col-lg-7 no-padding-left">
     <div class="v-box">
         <h4>Tin đăng <span class="v-user-properties-total"></span></h4>
         <div class="v-box-content v-user-properties open">
         </div>
     </div>
 </div>
+
+<div class="col-lg-5 no-padding-right">
+    <div class="v-box">
+        <h4>Thống kê</h4>
+        <div class="v-box-content open">
+            <div class="chart" style="margin-top:20px">
+                <canvas id="barChart" style="height:250px"></canvas>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="clearfix"></div>
