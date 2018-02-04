c4298817484a1960b00940012995aec1e80731e7
Tu Nguyen <tunguyenhsgs@gmail.com>
2018-02-05 00:13:21 +0700

    04.02.18

--- pages/templates/header.php
+++ pages/templates/header.php
@@ -17,7 +17,7 @@ $config->addJS('dist', 'main.js'); ?>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="shortcut icon" type="image/x-icon" href="<?php echo IMG ?>/b.jpg"/>
+	<link rel="shortcut icon" type="image/x-icon" href="<?php echo IMG ?>/marker4.png"/>
 
 	<title><?php echo $pageTitle ?></title>
 
