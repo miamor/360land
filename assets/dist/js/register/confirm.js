d5c01bf73ec41228602f54987aeb2b5a77c05a34
Tu Nguyen <tunguyenhsgs@gmail.com>
2018-03-12 00:15:32 +0700

    11.03.18

--- assets/dist/js/register/confirm.js
+++ assets/dist/js/register/confirm.js
@@ -1,8 +1,8 @@
 var __token = location.href.split('token=')[1].split('&')[0];
 
-function confirmAccount () {
+function confirmAccount() {
     $.ajax({
-        url: API_URL + '/user/confirm-email/'+__token+'/',
+        url: API_URL + '/user/confirm-email/' + __token + '/',
         type: 'get',
         //data: $(this).serialize(),
         /*beforeSend: function(xhr) {
@@ -11,19 +11,19 @@ function confirmAccount () {
         success: function (response) {
             console.log(response);
 
-	    if (response.data == 'OK') {
+            if (response.data == 'OK') {
                 localStorage.setItem('token', __token);
                 localStorage.setItem("login_time", Math.floor(Date.now() / 1000));
                 getUserInfo();
-		        mtip('.confirm-content', 'success', '', 'Xác thực người dùng thành công. Đang chuyển hướng...');
-		        locaton.href = MAIN_URL+'/manual';
+                mtip('.confirm-content', 'success', '', 'Xác thực người dùng thành công. Đang chuyển hướng...');
+                location.href = MAIN_URL + '/manual';
             }
         },
         error: function (a, b, c) {
             console.log(a)
             if (a.status == 401) {
                 //mtip('', 'error', '', 'Failed to authenticate');
-		        mtip('.confirm-content', 'error', '', 'Lối xác thực');
+                mtip('.confirm-content', 'error', '', 'Lối xác thực');
             }
             else {
                 mtip('.confirm-content', 'error', '', 'Lỗi hệ thống! Vui lòng liên hệ với quản trị viên để được hỗ trợ sớm nhất!');
@@ -34,10 +34,10 @@ function confirmAccount () {
 
 $(document).ready(function () {
     if (localStorage.getItem('user_info')) {
-	location.href = MAIN_URL;
+        location.href = MAIN_URL;
     } else if (!__token) {
-	mtip('', 'error', '', 'Failed to authenticate');
+        mtip('', 'error', '', 'Failed to authenticate');
     } else {
-	confirmAccount();
+        confirmAccount();
     }
 })
