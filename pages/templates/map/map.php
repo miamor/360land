c7b62e158e5fd38a439790a2cfbe64b209faa2a2
Tu Nguyen <tunguyenhsgs@gmail.com>
2018-04-02 12:33:51 +0700

    02.04.18

--- pages/templates/map/map.php
+++ pages/templates/map/map.php
@@ -28,22 +28,22 @@
         <div class="tab-content map-search-tabs">
             <div class="tab-pane active" id="map_search">
                 <ul class="nav nav-tabs map_search_select map-nav-select">
-                    <li class="active" attr-type="node"><a href="#map_search_node" data-toggle="tab">Rao bán/cho thuê</a></li>
-                    <li attr-type="project"><a href="#map_search_project" data-toggle="tab">Dự án</a></li>
+                    <li attr-type="node"><a href="#map_search_node" data-toggle="tab">Rao bán/cho thuê</a></li>
+                    <li class="active" attr-type="project"><a href="#map_search_project" data-toggle="tab">Dự án</a></li>
                 </ul>
 
                 <?php include 'form.search.php' ?>
             </div>
             <div class="tab-pane hide" id="map_results">
                 <ul class="nav nav-tabs map_results_select map-nav-select">
-                    <li class="active"><a href="#map_results_node" data-toggle="tab">Rao bán/cho thuê</a></li>
-                    <li><a href="#map_results_project" data-toggle="tab">Dự án</a></li>
+                    <li><a href="#map_results_node" data-toggle="tab">Rao bán/cho thuê</a></li>
+                    <li class="active"><a href="#map_results_project" data-toggle="tab">Dự án</a></li>
                 </ul>
                 <div class="tab-content map-results-tabs">
-                    <div class="tab-pane active" id="map_results_node">
+                    <div class="tab-pane hide" id="map_results_node">
                         <div class="empty_results">Không có bất động sản nào.</div>
                     </div>
-                    <div class="tab-pane hide" id="map_results_project">
+                    <div class="tab-pane active" id="map_results_project">
                         <div class="empty_results">Không có dự án nào.</div>
                     </div>
                 </div>
